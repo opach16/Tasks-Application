34755342da1f2353010beef40c010acfdf00b4ef
Konrad Opach <opach16@outlook.com>
2024-10-01 14:31:45 +0200

    17.4 Added Controller methods mapping

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,35 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/tasks")
+public class TaskController {
+
+    @GetMapping
+    public List<TaskDto> getTasks() {
+        return new ArrayList<TaskDto>();
+    }
+
+    @GetMapping(value = "/v1/tasks/{taskId}")
+    public TaskDto getTask(@PathVariable Long taskId) {
+        return new TaskDto(1L, "testTittle", "testContent");
+    }
+
+    @DeleteMapping
+    public void deleteTask(Long taskId) {
+    }
+
+    @PutMapping
+    public TaskDto updateTask(TaskDto taskDto) {
+        return new TaskDto(1L, "editedTestTittle", "editedTestContent");
+    }
+
+    @PostMapping
+    public void createTask(TaskDto taskDto) {
+    }
+}
